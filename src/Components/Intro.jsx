9f067abba5dc9a0bb9192d7144c61231582bac1f
Abdullahi Bashir Wale <bashabdulbash@gmail.com>
2023-08-07 23:35:17 +0200

    update

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,22 +4,22 @@ import { motion } from "framer-motion";
 function Intro() {
   return (
     <motion.div
-      className="flex flex-col justify-between text-yellow-100 px-5 sm:px-6 md:px-8 lg:px-12 pt-9"
+      className="flex flex-col justify-between text-yellow-100 px-5 sm:px-6 md:px-8 lg:px-12 pt-9 md:pt-4"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exist={{ opacity: 0 }}
     >
-      <h2 className="text-5xl font-semibold">
-        Hello! <span className="text-yellow-500 text-2xl">I'm Bashir,</span>
+      <h2 className="text-vw font-semibold">
+        Hello! <span className="text-yellow-500">I'm Bashir,</span>
       </h2>
-      <h1 className=" text-xl sm:2xl: md:text-3xl lg:text-4xl font-bold tracking-wide mt-4 mb-6">
+      <h1 className=" text-xl sm:2xl: md:text-3xl lg:text-4xl font-bold tracking-wide mt-2 mb-6 md:mb-8">
         Frontend Engineer
       </h1>
-      <p className=" text-sm md:text-base font-normal  max-w-lg">
-        Highly skilled and motivated front-end developer with a passion for
-        creating visually stunning and intuitive websites and web applications.
+      <p className=" text-sm md:text-base font-normal max-w-lg leading-6">
+        I'm a passionate and creative Frontend Engineer with a knack for turning
+        ideas into stunning digital experiences.
       </p>
-      <p className=" text-sm md:text-base font-normal mb-16 max-w-lg">
+      <p className=" text-sm md:text-base font-normal mb-24 md:mb-10 max-w-lg leading-6">
         With a deep love for clean code and pixel-perfect designs, I specialize
         in crafting user-friendly interfaces that leave a lasting impact.
       </p>
